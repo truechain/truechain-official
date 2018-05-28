547cc7734adbafe9e0c9808f89c24865f94aa283
Felix <2680383605@qq.com>
2018-05-28 14:49:11 +0800

    📍完成导航栏布局

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,17 +2,18 @@
   div
     nav
       .container-app-header
-        .container-app-header-logo
+        router-link(
+          to="/"
+        ).container-app-header-logo
           img(src="~@/assets/images/logo_top.png")
         .container-app-header-nav
           ul
-            router-link(
+            li(
               v-for="(item, index) in indexList",
               :key="index",
-              :to="item.path",
-              tag="li",
+              @click="jumpTo(item.path)",
             ) {{ $t(`nav.${item.tag}`) }}
-            li.container-app-header-lang(@click="toggleLanguage") ENGLISH▼
+            li.container-app-header-lang(@click="toggleLanguage") ENGLISH
     .container-app-content
       router-view
     .container-app-footer.
@@ -22,15 +23,14 @@
 
 <script>
 
-import { setStore, getStore } from '../util'
+import { setStore, getStore } from '@/util'
 
 const indexList = [
-  { path: 'join', tag: 'git' },
+  { path: 'https://github.com/truechain', tag: 'git' },
   { path: 'team', tag: 'about' },
-  { path: 'docs', tag: 'join' },
-  { path: 'message', tag: 'tec' },
-  { path: 'node', tag: 'docs' },
-  { path: 'node', tag: 'forum' },
+  { path: 'join', tag: 'join' },
+  { path: 'docs', tag: 'docs' },
+  { path: 'https://www.truechain.pro/', tag: 'forum' },
   { path: 'node', tag: 'noderank' }
 ]
 
@@ -41,9 +41,16 @@ export default {
     }
   },
   created () {
-    this._i18n.locale = getStore('locale')
+    this._i18n.locale = getStore('locale') || 'sc'
   },
   methods: {
+    jumpTo (path) {
+      if (/http[s]?:\/\//.test(path)) {
+        window.open(path)
+      } else {
+        this.$router.push(path)
+      }
+    },
     toggleLanguage () {
       if (getStore('locale') === 'sc') {
         setStore('locale', 'en')
@@ -63,10 +70,11 @@ export default {
     height 100px
     background-color $dark-blue
   .container-app-header
+    padding 35px
     mw()
   .container-app-header-logo
     wh(118px, 30px)
-    // background red
+    float left
   .container-app-header-nav
     color $font-light
     ul
@@ -74,15 +82,21 @@ export default {
       li
         float left
         color #fff
-        opacity .8
-        margin-left 40px
+        font-weight 200
+        opacity .6
+        margin-left 10px
         font-size 14px
         cursor pointer
-        transition opacity .4
+        transition opacity .4s
+        line-height 20px
+        padding 5px 15px
         &:hover
           opacity 1
       .container-app-header-lang
+        padding 4px 14px
         border solid 1px #fff
+        border-radius 15px
+        opacity 1
   .container-app-content
     background red
 </style>
