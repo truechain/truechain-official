5de1c7ca34ca26ab4fb151047473d268e8f8c289
baixiaojian <15101661380@163.com>
2018-05-28 09:41:41 +0800

    代码规范

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,6 @@
   </div>
 </template>
 <style lang="stylus">
-  @import './assets/stylus/base.styl';
-  @import './assets/stylus/reset.styl';
+  @import './assets/stylus/base.styl'
+  @import './assets/stylus/reset.styl'
 </style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -2,90 +2,86 @@
   .container-app
     .container-app-header
       .container-app-header-logo
-        //- img(src="~@/assets/images/logo-top.png")
-        //- img(src="../assets/images/logo-top.png")
+        img(src="~@/assets/images/logo_top.png")
       .container-app-header-nav
         ul
           router-link(
             v-for="item in list",
-            to="item.path",
+            :to="item.path",
             tag="li",
           ) {{item.name}}
-    div
+    div.aaaaa
       router-view
-    div
-    |  底部撒旦撒多
-  </div>
+
 </template>
 <script>
 export default {
-  data() {
+  data () {
     return {
       list: [
         {
-          path: "join",
-          name: "GITCLUB "
+          path: 'join',
+          name: 'GITCLUB'
         },
         {
-          path: "team",
-          name: "ABOUT "
+          path: 'team',
+          name: 'ABOUT '
         },
         {
-          path: "docs",
-          name: "JOIN US "
+          path: 'docs',
+          name: 'JOIN US '
         },
         {
-          path: "message",
-          name: "TECHNICAL"
+          path: 'message',
+          name: 'TECHNICAL'
         },
         {
-          path: "node",
-          name: "DOCS "
+          path: 'node',
+          name: 'DOCS '
         },
         {
-          path: "node",
-          name: "FORUM "
+          path: 'node',
+          name: 'FORUM '
         },
         {
-          path: "node",
-          name: "节点排行 "
+          path: 'node',
+          name: '节点排行 '
         },
         {
-          path: "node",
-          name: "ENGLISH▼"
+          path: 'node',
+          name: 'ENGLISH▼'
         }
       ]
-    };
+    }
   },
-  mounted() {
+  mounted () {
     // alert('aaa')
   }
-};
+}
 </script>
 <style lang="stylus">
 @import '~@/assets/stylus/mixin.styl'
-.container-app
-  // width 80%
-  // margin 0 auto
-.container-app-header
-  height 94px
-  display flex
-  fc(space-between)
-  padding 0 120px
-  background-color $dark-blue
-.container-app-header-logo
-  wh(118px, 30px)
-  background red
-.container-app-header-nav
-  color $font-light
-  ul
-    fj()
-    li
-      color #A9ADBB
-      margin-left 30px
-      font-size 14px
-.aaa
-  wh(100px, 30px)
-  border 1px solid white
-
+  .container-app
+  .container-app-header
+    height 94px
+    fc(space-between)
+    padding 0 60px
+    background green
+    background-color $dark-blue
+  .container-app-header-logo
+    wh(118px, 30px)
+    // background red
+  .container-app-header-nav
+    color $font-light
+    ul
+      fj()
+      li
+        color #A9ADBB
+        margin-left 30px
+        font-size 14px
+  .aaa
+    wh(100px, 30px)
+    border 1px solid white
+  .aaaaa
+    background red
 </style>
