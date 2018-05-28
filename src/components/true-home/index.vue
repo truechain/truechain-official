fd905a9fc45f63b5039169e359b410c54420eb92
Felix <2680383605@qq.com>
2018-05-28 18:19:33 +0800

    ⚙️Add banner

--- src/components/true-home/index.vue
+++ src/components/true-home/index.vue
@@ -1,5 +1,14 @@
-<template>
-  <div>
-    home
-  </div>
+<template lang="pug">
+  div
+    img(src="~@/assets/images/banner.png")
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus">
+
+</style>
