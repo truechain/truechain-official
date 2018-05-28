0f826794e9620f101fff8b008a414547001c2984
Felix <2680383605@qq.com>
2018-05-28 18:56:39 +0800

    ⚙️Add app download buttons, fix bug

---
+++ src/components/common/app-android.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="download-app-android">
+    <div class="button">ANDOOID</div>
+    <div class="qrcode"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-android'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/stylus/mixin.styl'
+
+.download-app-android
+  float left
+  margin 50px 30px 0 0
+  wh(150px, 50px)
+.button
+  border solid 1px #FFF
+  background-color #1000
+  border-radius 25px
+  line-height 50px
+  color #FFF
+  text-align center
+  font-size 14px
+</style>
---
+++ src/components/common/app-ios.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="download-app-ios">
+    <div class="button">IOS</div>
+    <div class="qrcode"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-ios'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/stylus/mixin.styl'
+
+.download-app-ios
+  float left
+  margin 50px 30px 0 0
+  wh(150px, 50px)
+.button
+  border solid 1px #FFF
+  background-color #1000
+  border-radius 25px
+  line-height 50px
+  color #FFF
+  text-align center
+  font-size 14px
+</style>
