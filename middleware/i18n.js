d501841727641ca17f5d0c4a3dfd7feaeb7e3333
baixiaojian <15101661380@163.com>
2018-06-21 10:50:10 +0800

    fix router jump

--- middleware/i18n.js
+++ middleware/i18n.js
@@ -1,24 +1,23 @@
 export default function ({ isHMR, app, store, route, params, error, redirect }) {
   const defaultLocale = app.i18n.fallbackLocale
+  debugger
   // If middleware is called from hot module replacement, ignore it
   if (isHMR) return
   // Get locale from params
+  const pathWhite = ['/news_detail', '/jd'];
   const locale = params.lang || defaultLocale
-  if (store.state.locales.indexOf(locale) === -1) {
+  if (store.state.locales.indexOf(locale) === -1 && !pathWhite.join(',').includes(locale)) {
     return error({ message: 'This page could not be found.', statusCode: 404 })
   }
-
-
 
   // Set locale
   store.commit('SET_LANG', locale)
   app.i18n.locale = store.state.locale
-
+  // debugger
   /* 清空路径中的不需要的值 */
   if(route.params.id && Number.isNaN(+(route.params.id))) {
-    debugger
+    // debugger
     const path = route.fullPath;
-    const pathWhite = ['/news_detail', '/jd'];
     const emptyPath = pathWhite.find(x => path.includes(x))
     if(emptyPath) {
       redirect(
