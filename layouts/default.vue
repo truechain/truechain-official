22be03151af6ac8b1f884d1d793ac249dccdc1f5
zihuatanejp <aimilixi@gmail.com>
2018-09-25 17:42:14 +0800

    update  truechain.pro stat

--- layouts/default.vue
+++ layouts/default.vue
@@ -208,6 +208,7 @@ export default {
   }
 }
 </script>
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1274893188'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1274893188%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 
 <style lang="stylus" scpoed>
 @import '~@/assets/stylus/index.styl';
