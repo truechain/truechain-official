d88955c346dd3acfa72b8cc229db605a20910f42
zihuatanejp <aimilixi@gmail.com>
2018-09-27 15:21:02 +0800

    update  bugfinder language

--- pages/_lang/bugfinder.vue
+++ pages/_lang/bugfinder.vue
@@ -5,18 +5,18 @@ div.finder-box
       div.left-logo
         img(:src="require(`~/assets/images/logo_bot.png`)")
       div
-        .finder-box-top-left-title 初链
-        .finder-box-top-left-descr 初链是真正快速、安全、不受限、可扩容的Minerva混合共识公链(无需许可链)，并拥有强大的全球开发者社区。
+        .finder-box-top-left-title {{ $t('bugfinder.pageTitle') }}
+        .finder-box-top-left-descr {{ $t('bugfinder.pageDescr') }}
     .finder-box-top-right
       .reward-item  
-        p.second-txt 最高奖励
+        p.second-txt {{ $t('bugfinder.highestRewardLabel') }}
         p.primary-txt 10000 TRUE        
       //- .reward-item
       //-   p.primary-txt 4 条
       //-   p.second-txt 已认领
   .finder-box-con
     .finder-box-left
-      .finder-box-left-title 测试范围
+      .finder-box-left-title {{ $t('bugfinder.testPlan') }}
       .finder-box-left-con
         pre.
           1. TrueChain Blockchain（GitHub 地址：https://github.com/truechain/truechain-engineering-code）
@@ -52,62 +52,48 @@ div.finder-box
           iii. Authentication system contract security
           2. TrueChain Client App
           a) True（https://github.com/truechain/truechain-light-wallet）
-      .finder-box-left-title 严重漏洞
+      .finder-box-left-title {{ $t('bugfinder.seriousBugLabel') }}
       .finder-box-left-con
         pre.
-          直接获取业务服务器权限的漏洞以及获取重要数据的漏洞，包括但不限于任意命令执行、上传webshell、任意代码执行、SQL注入获得大量数据；
-          严重影响系统业务安全的逻辑漏洞，包括但不限于任意帐号密码重置或更改漏洞，大量获取用户身份认证信息的漏洞，越权访问漏洞，未授权访问后台管理系统等；
-          可通过一定手段导致公链出现分叉；
-          当前阶段正在大规模爆发应引起足够重视的安全漏洞等。
-      .finder-box-left-title 高危漏洞
+          {{ $t('bugfinder.seriousBugText1') }}
+          {{ $t('bugfinder.seriousBugText2') }}
+          {{ $t('bugfinder.seriousBugText3') }}
+          {{ $t('bugfinder.seriousBugText4') }}
+      .finder-box-left-title {{ $t('bugfinder.highRiskBugLabel') }}
       .finder-box-left-con
         pre.
-          可通过一定手段严重影响业务运行，可能给客户带来巨大损失的，如关键业务操作可被Dos，登录接口可被撞库，业务系统可被轻易薅羊毛等；
-          需要用户交互才能获取用户身份信息的漏洞，包常规存储型／反射型XSS漏洞，核心关键业务操作的CSRF漏洞等；
-          任意文件读取、修改、覆盖、删除、下载；
-          绕过限制修改用户个人资料、个人信息、强制用户执行某些操作；
-          能够对目标系统带来危害的严重信息泄漏，敏感信息文件备份或源码泄露等（如存储密钥泄露，数据库连接密码泄露，SVN／Git帐号泄露，VPN帐号泄露等）。
-          能直接造成代币合约或用户资金损失的漏洞，如：能造成代币价值归零的数值溢出漏洞、能造成交易所损失代币的假充值漏洞、能造成合约账户损失ETH或代币的重入漏洞等；
-          能造成代币合约归属权丢失的漏洞，如：关键函数的访问控制缺陷、call注入导致关键函数访问控制绕过等；
-          能造成代币合约无法正常工作的漏洞，如：因向恶意地址发送ETH导致的拒绝服务漏洞、因gas耗尽导致的拒绝服务漏洞。
+          {{ $t('bugfinder.highRiskBugText') }}
 
-      .finder-box-left-title 中危漏洞
+      .finder-box-left-title {{ $t('bugfinder.midRiskBugLabel') }}
       .finder-box-left-con
         pre.
-          能够对目标系统的安全造成影响但无法直接证实可被利用的；
-          能够获取目标网络设备权限但确定无法进一步利用的；
-          非重要信息泄露、开放的URL跳转漏洞、有一定限制且确定较难利用的XSS漏洞、非核心关键业务操作的CSRF漏洞等。
-          需要特定地址才能触发的高风险漏洞，如代币合约拥有者才能触发的数值溢出漏洞等；非关键函数的访问控制缺陷、不能造成直接资金损失的逻辑设计缺陷等。
+          {{ $t('bugfinder.midRiskBugText') }}
 
-      .finder-box-left-title 低危漏洞
+      .finder-box-left-title {{ $t('bugfinder.lowRiskBugLabel') }}
       .finder-box-left-con
         pre.
-          无法确定是否能够对目标系统的安全造成影响的，如目标系统管理端口向公网开放但无法直接利用，目标系统Banner信息可被识别，以及其他渗透测试人员认定，较难利用但可能会存在潜在安全威胁的漏洞。
-          难以被触发的漏洞、触发之后危害有限的漏洞，如需要大量ETH或代币才能触发的数值溢出漏洞、触发数值溢出后攻击者无法直接获利的漏洞、通过指定高gas触发的事务顺序依赖风险等。
+          {{ $t('bugfinder.lowRiskBugText') }}
 
-      .finder-box-left-title 注意事项
+      .finder-box-left-title {{ $t('bugfinder.noticeLabel') }}
       .finder-box-left-con
         pre.
-          • 客户端包括所有通过官方途径发布的客户端程序；
-          • 提交在其他漏洞披露平台已提交的漏洞不计奖励。
-          • 开放平台的第三方应用漏洞不计奖励；
-          • 同一漏洞最早提交者得奖励。
+          {{ $t('bugfinder.noticeText') }}
     .finder-box-right
       .finder-box-right-title
-        span 奖励计划
-        a.btn(href="https://dvpnet.io/company/company-info/id/99#item-three") 提交漏洞
+        span {{ $t('bugfinder.rewardPlan') }}
+        a.btn(href="https://dvpnet.io/company/company-info/id/99#item-three") {{ $t('bugfinder.submitBug') }}
       .finder-box-right-item
         span.dot.serious
-        span.text 严重：2700 - 10000 TRUE
+        span.text {{ $t('bugfinder.seriousBugLabel') }}：2700 - 10000 TRUE
       .finder-box-right-item
         span.dot.high
-        span.text 高危：1300 - 2700 TRUE
+        span.text {{ $t('bugfinder.highRiskBugLabel') }}：1300 - 2700 TRUE
       .finder-box-right-item
         span.dot.mid
-        span.text 中危：80 - 1300 TRUE
+        span.text {{ $t('bugfinder.midRiskBugLabel') }}：80 - 1300 TRUE
       .finder-box-right-item
         span.dot.low
-        span.text 低危：20 - 80 TRUE
+        span.text {{ $t('bugfinder.lowRiskBugLabel') }}：20 - 80 TRUE
         
 </template>
 
@@ -155,6 +141,7 @@ export default {
       font-size 14px
       line-height 20px
       color #798d99
+      max-width 700px
     .finder-box-top-right
       margin 0px 20px
       .reward-item
