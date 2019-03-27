43428b3b4c171595adac4c7486a485584fd9bcd5
tari404 <2680383605@qq.com>
2019-03-27 15:00:20 +0800

    ⚙️ fix bug

--- middleware/i18n.js
+++ middleware/i18n.js
@@ -1,5 +1,5 @@
 export default function ({ isHMR, app, store, route, params, error, redirect }) {
-  const defaultLocale = app.i18n.fallbackLocale
+  const defaultLocale = 'zh'
   // debugger
   // If middleware is called from hot module replacement, ignore it
   if (isHMR) return
